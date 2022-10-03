51d400627a3991a277305ce7fd988fd0f34c198e
Miguel Polonio <61948141+m-polonio@users.noreply.github.com>
2022-10-03 22:01:36 +0200

    20221003 v1

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import app from './server.js';
 import mongodb from "mongodb";
 import dotenv from "dotenv";
+import MoviesDAO from './dao/moviesDAO.js';
 
 async function main() {
     dotenv.config()
@@ -10,6 +11,7 @@ async function main() {
     try {
         //Connect to the MondoDB cluster
         await client.connect()
+        await MoviesDAO.injectDB(client)
         app.listen(port, () => {
             console.log('server is running on port:' + port);
         })
